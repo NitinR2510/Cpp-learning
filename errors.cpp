c31aa6a1ff6f9fe73d737e4b5cb8008eb3382afc
Nitin Ranjan <43118810+NitinR2510@users.noreply.github.com>
2020-07-01 02:50:50 +0530

    updated variables

---
+++ errors.cpp
@@ -0,0 +1,11 @@
+//Errors in cpp
+/*
+Syntax error
+Logical error
+Semantic error
+Runtime error
+Runtime error
+Liker error
+*/
+
+
--- variables2.cpp
+++ variables2.cpp
@@ -4,6 +4,13 @@ int x; //global varibale
 int main()
 {
 	//int x=10;  //variable is searched for in the closest scope. 
-	cout<<x;  //default value of global variable is 0.
+	cout<<x<<endl;  //default value of global variable is 0.
+	//new variable with same name in same scope only.
+	static int y;//will remain in the program throughout the run. Does not change with scope.
+	cout<<y<<endl;
+	//static local var like global var are intialized at 0.
+	const int z = 10;//value cannot be changed
+	//z+=1; compiler error
+	cout<<z<<endl; //const recommended over macros
 	return 0;
 }
